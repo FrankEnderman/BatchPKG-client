ff7557eda82716fe1a5ed414e842267718dce98b
Trololo <90335574+FrankEnderman@users.noreply.github.com>
2022-02-09 08:50:19 +0530

    add install feature

--- BStore.bat
+++ BStore.bat
@@ -6,9 +6,10 @@ cls
 echo Welcome to BatchStore
 echo NOTE: The app name is case-sensitive
 set /p app= Application:
-echo Code for The Application. Copy and paste into a Batch File to run
+
 echo.
-curl https://raw.githubusercontent.com/FrankEnderman/BatchPKG/main/%app%.bat
+curl https://raw.githubusercontent.com/FrankEnderman/BatchPKG/main/%app%.bat >> %app%.bat
 echo.
+echo The file has been downloaded in the same folder as the program
 pause
 goto appstore
