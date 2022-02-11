136218668895db9081e4db3f8241fcb2e5b567fa
Trololo <90335574+FrankEnderman@users.noreply.github.com>
2022-02-11 09:39:19 +0530

    SP 2
    last update

--- BStore.bat
+++ BStore.bat
@@ -6,11 +6,13 @@ cls
 curl https://raw.githubusercontent.com/FrankEnderman/BatchPKG-client/1.1/announcements
 echo --------------------------------------
 echo NOTE: The app name is case-sensitive
+echo --------------------------------------
+curl https://raw.githubusercontent.com/FrankEnderman/BatchPKG-client/1.1/list
 set /p app= Application:
 
 echo.
 curl https://raw.githubusercontent.com/FrankEnderman/BatchPKG/main/%app%.bat >> %app%.bat
 echo.
-echo The file has been downloaded in the same folder as the program
+echo The file has been downloaded in the same folder as BatchPKG
 pause
 goto appstore
